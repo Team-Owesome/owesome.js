78cb8cf28037209f7b2a48b8e32781e72ee32bb7
Till Hagger <till.hagger@dudagroup.com>
2014-06-27 19:30:03 +0200

    Sprite batching success!

--- build/main.js
+++ build/main.js
@@ -1,6 +1,8 @@
 var lw = {};
+lw.SPRITE_VERT_SHADER_SRC = 'uniform mat4 projectionMatrix;attribute vec4 vertex;varying highp vec2 vTextureCoord;void main() { gl_Position = projectionMatrix * vec4(vertex.xy, 0, 1); vTextureCoord = vec2(vertex.zw);}';
 lw.SPRITE_FRAG_SHADER_SRC = 'uniform sampler2D texture;varying highp vec2 vTextureCoord;void main(){ gl_FragColor = texture2D(texture, vTextureCoord);}';
-lw.SPRITE_VERT_SHADER_SRC = 'attribute vec2 position;varying highp vec2 vTextureCoord;void main() { gl_Position = vec4(position, 0, 1); vTextureCoord = vec2(gl_Position);}';
+"use strict";
+
 (function()
 {
     var canvas = document.createElement('canvas');
@@ -35,26 +37,72 @@ lw.SPRITE_VERT_SHADER_SRC = 'attribute vec2 position;varying highp vec2 vTexture
     console.log('TEEST');
 
     // look up where the vertex data needs to go.
-    var positionLocation = gl.getAttribLocation(program, "position");
+    var positionLocation = gl.getAttribLocation(program, "vertex");
+    var matrixLocation = gl.getUniformLocation(program, 'projectionMatrix');
+
+
+    gl.uniformMatrix4fv(matrixLocation, false,
+    [
+        2 / canvas.width, 0, 0, 0,
+        0, -2 / canvas.height, 0, 0,
+        0, 0, 2 / 1, 0,
+        -1, 1, 0, 1,
+    ]);
+
+
+
+
+    var setBuffer = function(buffer, index, x, y, width, height)
+    {
+        buffer[(index * 24) + 0] = x;
+        buffer[(index * 24) + 1] = y;
+        buffer[(index * 24) + 2] = 0;
+        buffer[(index * 24) + 3] = 0;
+
+        buffer[(index * 24) + 4] = x;
+        buffer[(index * 24) + 5] = y + height;
+        buffer[(index * 24) + 6] = 0;
+        buffer[(index * 24) + 7] = 1;
+
+        buffer[(index * 24) + 8] = x + width;
+        buffer[(index * 24) + 9] = y;
+        buffer[(index * 24) + 10] = 1;
+        buffer[(index * 24) + 11] = 0;
+
+        buffer[(index * 24) + 12] = x + width;
+        buffer[(index * 24) + 13] = y + height;
+        buffer[(index * 24) + 14] = 1;
+        buffer[(index * 24) + 15] = 1;
+
+        buffer[(index * 24) + 16] = x + width;
+        buffer[(index * 24) + 17] = y;
+        buffer[(index * 24) + 18] = 1;
+        buffer[(index * 24) + 19] = 0;
+
+        buffer[(index * 24) + 20] = x;
+        buffer[(index * 24) + 21] = y + height;
+        buffer[(index * 24) + 22] = 0;
+        buffer[(index * 24) + 23] = 1;
+
+
+
+        /*return [
+            x, y, 0, 0,
+            x, y + height, 0, 1,
+            x + width, y, 1, 0,
+
+            x + width, y + height, 1, 1,
+            x + width, y, 1, 0,
+            x, y + height, 0, 1
+        ];*/
+    };
+
+
+  
 
     // Create a buffer and put a single clipspace rectangle in
     // it (2 triangles)
     var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(
-        gl.ARRAY_BUFFER, 
-        new Float32Array(
-        [
-            -1.0, -1.0,
-            -1.0, 0.0,
-            0.0, -1.0,
-            0.0, -1.0,
-            -1.0, 0.0,
-            0.0, 0.0
-        ]), 
-        gl.STATIC_DRAW);
-    gl.enableVertexAttribArray(positionLocation);
-    gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
     var texture = gl.createTexture();
     var image = new Image();
@@ -70,21 +118,37 @@ lw.SPRITE_VERT_SHADER_SRC = 'attribute vec2 position;varying highp vec2 vTexture
         gl.bindTexture(gl.TEXTURE_2D, null);
     };
 
+    var floatBuffer = new Float32Array(10000 * 24);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, 
+                      floatBuffer, 
+                      gl.DYNAMIC_DRAW);
+
     window.requestAnimationFrame(function render()
     {
         window.requestAnimationFrame(render);
 
         if (!image.complete) return;
 
+        for (var i = 0; i < 10000; i++)
+        {
+            setBuffer(floatBuffer, i,
+                      Math.random() * 300, Math.random() * 300,
+                      Math.random() * 64, Math.random() * 64)
+        }
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, floatBuffer);
+
+        gl.enableVertexAttribArray(positionLocation);
+        gl.vertexAttribPointer(positionLocation, 4, gl.FLOAT, false, 0, 0);
+
         gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
 
-        // draw
-        for (var i = 0; i < 2000; i++)
-        {
-            gl.drawArrays(gl.TRIANGLES, 0, 6);
-        }
+        gl.drawArrays(gl.TRIANGLES, 0, 6 * 10000);
     });
 
     document.body.appendChild(canvas);
--- src/shaders/sprite.vert
+++ src/shaders/sprite.vert
@@ -1,11 +1,13 @@
 // lw.SPRITE_VERT_SHADER_SRC
 
-attribute vec2 position;
+uniform mat4 projectionMatrix;
+
+attribute vec4 vertex;
 
 varying highp vec2 vTextureCoord;
 
 void main() 
 {
-    gl_Position = vec4(position, 0, 1);
-    vTextureCoord = vec2(gl_Position);
+    gl_Position = projectionMatrix * vec4(vertex.xy, 0, 1);
+    vTextureCoord = vec2(vertex.zw);
 }
