08caea8da0aff6ab760beb3f82d94380b47d2233
Till Hagger <till.hagger@dudagroup.com>
2014-07-25 16:04:14 +0200

    Removed old functions, fixed some unused functions.

--- src/Matrix.js
+++ src/Matrix.js
@@ -15,14 +15,14 @@
     {
         var ar = this.array;
 
-        return [ar[0], ar[1], ar[2],
-                ar[3], ar[4], ar[5],
-                ar[6], ar[7], ar[8]];
+        return [this.a, this.b, this.tx,
+                this.c, this.d, this.ty,
+                0,      0,      1];
     };
 
     Matrix.prototype.toNativeArray = function()
     {
-        return new Float32Array(this.array);
+        return new Float32Array(this.toArray());
     };
 
     Matrix.prototype.copy = function()
@@ -85,36 +85,6 @@
 
         return this;
     }
-
-    Matrix.prototype.multiplyByMatrixArray = function(array)
-    {
-        var aa = this.array;
-        var ba = array;
-
-        var ta = this.array;
-
-        var a11 = aa[0]; var a12 = aa[1]; var a13 = aa[2];
-        var a21 = aa[3]; var a22 = aa[4]; var a23 = aa[5];
-        var a31 = aa[6]; var a32 = aa[7]; var a33 = aa[8];
-
-        var b11 = ba[0]; var b12 = ba[1]; var b13 = ba[2];
-        var b21 = ba[3]; var b22 = ba[4]; var b23 = ba[5];
-        var b31 = ba[6]; var b32 = ba[7]; var b33 = ba[8];
-
-        ta[0] = (a11 * b11) + (a12 * b21) + (a13 * b31);
-        ta[1] = (a11 * b12) + (a12 * b22) + (a13 * b32);
-        ta[2] = (a11 * b13) + (a12 * b23) + (a13 * b33);
-
-        ta[3] = (a21 * b11) + (a22 * b21) + (a23 * b31);
-        ta[4] = (a21 * b12) + (a22 * b22) + (a23 * b32);
-        ta[5] = (a21 * b13) + (a22 * b23) + (a23 * b33);
-
-        ta[6] = (a31 * b11) + (a32 * b21) + (a33 * b31);
-        ta[7] = (a31 * b12) + (a32 * b22) + (a33 * b32);
-        ta[8] = (a31 * b13) + (a32 * b23) + (a33 * b33);
-
-        return this;
-    };
 
     Matrix.prototype.multiply = function(matrix)
     {
