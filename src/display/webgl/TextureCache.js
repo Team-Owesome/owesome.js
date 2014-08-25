c50a7778099715822499436907de40a75b13a8a6
Till Hagger <till.hagger@gmail.com>
2014-08-25 22:13:42 +0200

    Removed mipmapping.

--- src/display/webgl/TextureCache.js
+++ src/display/webgl/TextureCache.js
@@ -43,9 +43,11 @@ TextureCache.prototype.getGlTexture = function(texture)
 
         gl.bindTexture(gl.TEXTURE_2D, glTexture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.getImage());
+
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-        gl.generateMipmap(gl.TEXTURE_2D);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        
+        //gl.generateMipmap(gl.TEXTURE_2D);
         gl.bindTexture(gl.TEXTURE_2D, null);
 
         this._glTextures[texture.getId()] = glTexture;
